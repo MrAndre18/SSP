938dd11ecd6d703b483d72cd9646626fb50bb3f7
MrAndre <48051685+MrAndre18@users.noreply.github.com>
2021-03-15 19:54:25 +0300

    Update

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 // /* src/app.js */
-import AOS from 'aos';
-import Rellax from 'rellax';
-import Sticky from 'sticky-js';
+import AOS from "aos";
+import Rellax from "rellax";
+import Sticky from "sticky-js";
 // import Swiper from 'swiper/swiper-bundle.min';
-import 'velocity-animate';
+import "velocity-animate";
 // import './assets/scripts/backend.js';
 // import './assets/scripts/map.js';
 // import './assets/scripts/tab-nav.js';
-import 'jquery';
+import "jquery";
 
 // Styles
-import 'Styles/_app.scss';
+import "Styles/_app.scss";
 
-$(document).ready(() => {
+$(() => {
   require("Scripts/convert-svg");
   require("Scripts/sliders");
 });
@@ -46,160 +46,154 @@ $(document).ready(() => {
 
 // // MAIN PAGE: loader
 
-// $(() => {
-// 	const loaderCounter = $('.loader span');
-// 	const loaderLayer = $('.loader__layer');
+$(() => {
+  const loader = $(".loader"),
+    progress = loader.find(".loader-progress"),
+    circle = progress.find(".loader-progress__circle"),
+    radius = +circle.attr("r"),
+    circumference = 2 * Math.PI * radius;
 
-// 	const getMainUrl = window.location.pathname === '/' || window.location.pathname === '/index' || window.location.pathname === '/index.html';
-// 	console.log(getMainUrl);
+  if (process.env.NODE_ENV === "production" || !loader.hasClass("hidden")) {
+    setTimeout(function () {
+      window.scrollTo(0, 0);
+    }, 200);
+  }
 
-// 	if (process.env.NODE_ENV === 'production' || $('.summerdream').length) {
-// 		setTimeout(function () {
-// 			window.scrollTo(0, 0);
-// 		}, 200);
-// 	}
+  if (!loader.hasClass("hidden")) {
+    // $(".loaded-content").css("display", "none");
+    $("body").css("overflow", "hidden");
+    circle.css("strokeDasharray", `${circumference} ${circumference}`);
+    circle.css("strokeDashoffset", circumference);
 
+    // параметры прелоадера
+    const // информация об изображениях
+      img = {
+        elem: $("img"),
+        count: $("img").length,
+      };
+    // информация о загрузке изображений
+    const load = {
+      counter: 0,
+      startTime: null,
+      endTime: null,
+      time: null,
+      progress: null,
+      TAIL: {
+        MIN_TIME: 200,
+        MAX_TIME: 500,
+        PARTS: 5,
+        BREAKPOINT: 0.6 + 0.2 * Math.random(),
+      },
+    };
 
-// 	if ($('.summerdream').length) {
-// 		$('.index-loader').css('display', 'none');
-// 		$('body').css('overflow', 'hidden');
+    // загрузка одного изображения (тэг img)
+    img.elem.on("load", () => {
+      load.counter++;
 
+      if (!load.startTime) {
+        load.startTime = new Date().getTime();
+      }
 
-// 		// параметры прелоадера
-// 		const
-// 			// информация об изображениях
-// 			img = {
-// 				elem: $('img'),
-// 				count: $('img').length
-// 			};
-// 		// информация о загрузке изображений
-// 		const load = {
-// 			counter: 0,
-// 			startTime: null,
-// 			endTime: null,
-// 			time: null,
-// 			progress: null,
-// 			TAIL: {
-// 				MIN_TIME: 200,
-// 				MAX_TIME: 500,
-// 				PARTS: 5,
-// 				BREAKPOINT: 0.6 + 0.2 * Math.random()
-// 			}
-// 		};
+      load.progress = load.counter / img.count;
+      load.progress =
+        load.progress < load.TAIL.BREAKPOINT
+          ? load.progress
+          : load.TAIL.BREAKPOINT;
 
-// 		// загрузка одного изображения (тэг img)
-// 		img.elem.on('load', () => {
-// 			load.counter++;
+      preloaderUpdate(load.progress * 0.5);
+    });
 
-// 			if (!load.startTime) {
-// 				load.startTime = (new Date()).getTime();
-// 			}
+    // загрузка всей страницы (всех изображений)
+    $(window).on("load", () => {
+      load.progress = load.progress <= 0 ? load.TAIL.BREAKPOINT : load.progress;
 
-// 			load.progress = load.counter / img.count;
-// 			load.progress = (load.progress < load.TAIL.BREAKPOINT) ? load.progress : load.TAIL.BREAKPOINT;
+      if (load.progress >= 1) {
+        preloaderClose();
+      } else {
+        load.endTime = new Date().getTime();
 
-// 			preloaderUpdate(load.progress * 0.5);
-// 		});
+        load.time = load.endTime - load.startTime;
+        load.time = load.time > 0 ? load.time : 0;
 
-// 		// загрузка всей страницы (всех изображений)
-// 		$(window).on('load', () => {
-// 			load.progress = (load.progress <= 0) ? load.TAIL.BREAKPOINT : load.progress;
+        const tail = {
+          time: null,
+          partCounter: 0,
+          partTime: null,
+          interval: null,
+        };
 
-// 			if (load.progress >= 1) {
-// 				preloaderClose();
-// 			} else {
-// 				load.endTime = (new Date()).getTime();
+        tail.time = (load.time / load.progress) * (1 - load.progress);
+        tail.time =
+          tail.time < load.TAIL.MAX_TIME
+            ? tail.time < load.TAIL.MIN_TIME
+              ? load.TAIL.MIN_TIME
+              : tail.time
+            : load.TAIL.MAX_TIME;
 
-// 				load.time = load.endTime - load.startTime;
-// 				load.time = (load.time > 0) ? load.time : 0;
+        tail.partCounter = 0;
+        tail.partTime = tail.time / load.TAIL.PARTS;
+        tail.interval = setInterval(() => {
+          tail.partCounter++;
 
-// 				const tail = {
-// 					time: null,
-// 					partCounter: 0,
-// 					partTime: null,
-// 					interval: null
-// 				};
+          if (tail.partCounter < load.TAIL.PARTS) {
+            preloaderUpdate(
+              load.progress +
+                ((1 - load.progress) / load.TAIL.PARTS) * tail.partCounter
+            );
+          } else {
+            preloaderClose();
 
-// 				tail.time = (load.time / load.progress) * (1 - load.progress);
-// 				tail.time = (tail.time < load.TAIL.MAX_TIME) ? ((tail.time < load.TAIL.MIN_TIME) ? load.TAIL.MIN_TIME : tail.time) : load.TAIL.MAX_TIME;
+            clearInterval(tail.interval);
+          }
+        }, tail.partTime);
+      }
+    });
 
-// 				tail.partCounter = 0;
-// 				tail.partTime = tail.time / load.TAIL.PARTS;
-// 				tail.interval = setInterval(() => {
-// 					tail.partCounter++;
+    // обновление прелоадера (отображение прогресса)
+    function preloaderUpdate(loadProgress) {
+      const loadProgressPercent = Math.round(loadProgress * 100);
 
-// 					if (tail.partCounter < load.TAIL.PARTS) {
-// 						preloaderUpdate(load.progress + (1 - load.progress) / load.TAIL.PARTS * tail.partCounter);
-// 					} else {
-// 						preloaderClose();
+      const offset =
+        circumference - (loadProgressPercent / 100) * circumference;
+      circle.css("strokeDashoffset", offset);
+    }
 
-// 						clearInterval(tail.interval);
-// 					}
-// 				}, tail.partTime);
-// 			}
-// 		});
+    // закрытие прелоадера
+    function preloaderClose() {
+      $(".loaded-content").css("display", "block");
+      circle.css("strokeDashoffset", 0);
 
-// 		// обновление прелоадера (отображение прогресса)
-// 		function preloaderUpdate(loadProgress) {
-// 			const loadProgressPercent = Math.round(loadProgress * 100);
+      // OLD CODE START
 
-// 			loaderCounter.text(loadProgressPercent);
-// 			loaderLayer.css('width', loadProgressPercent + '%');
-// 		}
+      $("body").css("overflow", "visible");
 
-// 		// закрытие прелоадера
-// 		function preloaderClose() {
-// 			$('.index-loader').css('display', '');
-// 			loaderCounter.text('100');
-// 			loaderLayer.css('width', '100%');
+      setTimeout(() => {
+        $(".loader").addClass("hidden");
+        AOS.init({ offset: 50 });
+      }, 500);
 
+      if ($(".rellax").length) {
+        // eslint-disable-next-line no-new
+        new Rellax(".rellax");
+      }
 
-// 			// OLD CODE START
+      // OLD CODE END
+    }
+  } else {
+    $("body").css("overflow", "hidden");
 
-// 			$('body').css('overflow', 'visible');
-// 			$('.summerdream').addClass('hidden');
+    setTimeout(() => {
+      $("body").css("overflow", "visible");
 
-// 			setTimeout(() => {
-// 				$('.fullpage-block').addClass('active');
-// 				$('.summerdream').addClass('hideIt');
-// 				$('.fullpage-desc span:first-child').addClass('active');
-// 				setTimeout(() => {
-// 					$('.fullpage-desc span:nth-child(3)').addClass('active');
-// 					setTimeout(() => {
-// 						$('.fullpage-desc span:last-child').addClass('active');
-// 					}, 300);
-// 				}, 300);
-// 			}, 500);
+      AOS.init({ offset: 50 });
 
-// 			setTimeout(() => {
-// 				AOS.init({ offset: 50 });
-// 			}, 500);
-
-// 			if ($('.rellax').length) {
-// 				// eslint-disable-next-line no-new
-// 				new Rellax('.rellax');
-// 			}
-
-// 			// OLD CODE END
-// 		}
-
-
-// 	} else {
-// 		$('body').css('overflow', 'hidden');
-
-// 		setTimeout(() => {
-// 			$('.loading').addClass('hideIt');
-// 			$('body').css('overflow', 'visible');
-
-// 			AOS.init({ offset: 50 });
-
-// 			if ($('.rellax').length) {
-// 				// eslint-disable-next-line no-new
-// 				new Rellax('.rellax');
-// 			}
-// 		}, 500);
-// 	}
-// });
+      if ($(".rellax").length) {
+        // eslint-disable-next-line no-new
+        new Rellax(".rellax");
+      }
+    }, 500);
+  }
+});
 
 // {
 // 	$(() => {
--- src/assets/scripts/sliders.js
+++ src/assets/scripts/sliders.js
@@ -17,7 +17,6 @@ const practicesSlider = new Swiper(".practices__slider", {
 
 const fullPageSlider = new Swiper(".fulpage__slider", {
   loop: true,
-  loopedSlides: 3,
   effect: "fade",
   speed: 700,
   simulateTouch: false,
@@ -26,4 +25,76 @@ const fullPageSlider = new Swiper(".fulpage__slider", {
     delay: 5000,
     disableOnInteraction: false,
   },
+
+  // pagination: {
+  //   el: ".fulpage__slider-pagination",
+  //   type: "bullets",
+  //   clickable: true,
+  //   // renderBullet: function (index, className) {
+  //   //   return (
+  //   //     "<div class='fulpage__slider-bullet " +
+  //   //     className +
+  //   //     "'>" +
+  //   //     "<span class='bullet-content'></span>" +
+  //   //     "<svg class='bullet-progress' width='220' height='220'>" +
+  //   //     "<circle class='bullet-progress__circle' stroke='#fff' stroke-width='1' cx='104' cy='104' r='102' fill='transparent'>" +
+  //   //     "</circle>" +
+  //   //     "</svg>" +
+  //   //     "</div>"
+  //   //   );
+  //   // },
+  // },
+  // on: {
+  //   afterInit: function () {
+  //     setCircleFilling();
+  //   },
+  //   slideChangeTransitionStart: function () {
+  //     setCircleFilling();
+  //   },
+  // },
 });
+
+$(".fulpage__slider-bullet").on('click', e => {
+  let bullet;
+  if ($(e.target).hasClass("fulpage__slider-bullet")) {
+    bullet = e.target;
+  } else {
+    bullet = e.target.closest(".fulpage__slider-bullet");
+  }
+
+  const index = $(bullet).data("bulletIndex");
+  console.log('index: ', index);
+
+  fullPageSlider.slideToLoop(index, 300, true);
+});
+
+// function setCircleFilling() {
+//   const bullets = $(".swiper-pagination-bullet");
+//   $(bullets).each(function (index, bullet) {
+//     if (!$(bullet).hasClass("swiper-pagination-bullet-active")) {
+//       const progress = $(bullet).find(".bullet-progress"),
+//         circle = progress.find(".bullet-progress__circle"),
+//         radius = +circle.attr("r"),
+//         circumference = 2 * Math.PI * radius;
+
+//       circle.css("strokeDasharray", `${circumference} ${circumference}`);
+//       circle.css("strokeDashoffset", circumference);
+//     } else {
+//       const activeBullet = $(".swiper-pagination-bullet-active"),
+//         progress = activeBullet.find(".bullet-progress"),
+//         circle = progress.find(".bullet-progress__circle"),
+//         radius = +circle.attr("r"),
+//         circumference = 2 * Math.PI * radius,
+//         delay = 5000;
+
+//       filingCircle(100, circumference, circle);
+//     }
+//   });
+// }
+
+// function filingCircle(percent, circumference, circle) {
+//   const offset = circumference - (percent / 100) * circumference;
+//   console.log("offset: ", offset);
+//   circle.css("strokeDashoffset", offset);
+//   console.log("circle: ", circle.css("strokeDashoffset"));
+// }
