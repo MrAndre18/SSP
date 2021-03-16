49d82bb41205c2b802d8b692c0f9fc6a030bace0
MrAndre <48051685+MrAndre18@users.noreply.github.com>
2021-03-16 11:27:26 +0300

    update

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ import "Styles/_app.scss";
 $(() => {
   require("Scripts/convert-svg");
   require("Scripts/sliders");
+  require("Scripts/scroll");
 });
 
 // $(document).ready(() => {
@@ -46,154 +47,154 @@ $(() => {
 
 // // MAIN PAGE: loader
 
-$(() => {
-  const loader = $(".loader"),
-    progress = loader.find(".loader-progress"),
-    circle = progress.find(".loader-progress__circle"),
-    radius = +circle.attr("r"),
-    circumference = 2 * Math.PI * radius;
+// $(() => {
+//   const loader = $(".loader"),
+//     progress = loader.find(".loader-progress"),
+//     circle = progress.find(".loader-progress__circle"),
+//     radius = +circle.attr("r"),
+//     circumference = 2 * Math.PI * radius;
 
-  if (process.env.NODE_ENV === "production" || !loader.hasClass("hidden")) {
-    setTimeout(function () {
-      window.scrollTo(0, 0);
-    }, 200);
-  }
+//   if (process.env.NODE_ENV === "production" || !loader.hasClass("hidden")) {
+//     setTimeout(function () {
+//       window.scrollTo(0, 0);
+//     }, 200);
+//   }
 
-  if (!loader.hasClass("hidden")) {
-    // $(".loaded-content").css("display", "none");
-    $("body").css("overflow", "hidden");
-    circle.css("strokeDasharray", `${circumference} ${circumference}`);
-    circle.css("strokeDashoffset", circumference);
+//   if (!loader.hasClass("hidden")) {
+//     // $(".loaded-content").css("display", "none");
+//     $("body").css("overflow", "hidden");
+//     circle.css("strokeDasharray", `${circumference} ${circumference}`);
+//     circle.css("strokeDashoffset", circumference);
 
-    // параметры прелоадера
-    const // информация об изображениях
-      img = {
-        elem: $("img"),
-        count: $("img").length,
-      };
-    // информация о загрузке изображений
-    const load = {
-      counter: 0,
-      startTime: null,
-      endTime: null,
-      time: null,
-      progress: null,
-      TAIL: {
-        MIN_TIME: 200,
-        MAX_TIME: 500,
-        PARTS: 5,
-        BREAKPOINT: 0.6 + 0.2 * Math.random(),
-      },
-    };
+//     // параметры прелоадера
+//     const // информация об изображениях
+//       img = {
+//         elem: $("img"),
+//         count: $("img").length,
+//       };
+//     // информация о загрузке изображений
+//     const load = {
+//       counter: 0,
+//       startTime: null,
+//       endTime: null,
+//       time: null,
+//       progress: null,
+//       TAIL: {
+//         MIN_TIME: 200,
+//         MAX_TIME: 500,
+//         PARTS: 5,
+//         BREAKPOINT: 0.6 + 0.2 * Math.random(),
+//       },
+//     };
 
-    // загрузка одного изображения (тэг img)
-    img.elem.on("load", () => {
-      load.counter++;
+//     // загрузка одного изображения (тэг img)
+//     img.elem.on("load", () => {
+//       load.counter++;
 
-      if (!load.startTime) {
-        load.startTime = new Date().getTime();
-      }
+//       if (!load.startTime) {
+//         load.startTime = new Date().getTime();
+//       }
 
-      load.progress = load.counter / img.count;
-      load.progress =
-        load.progress < load.TAIL.BREAKPOINT
-          ? load.progress
-          : load.TAIL.BREAKPOINT;
+//       load.progress = load.counter / img.count;
+//       load.progress =
+//         load.progress < load.TAIL.BREAKPOINT
+//           ? load.progress
+//           : load.TAIL.BREAKPOINT;
 
-      preloaderUpdate(load.progress * 0.5);
-    });
+//       preloaderUpdate(load.progress * 0.5);
+//     });
 
-    // загрузка всей страницы (всех изображений)
-    $(window).on("load", () => {
-      load.progress = load.progress <= 0 ? load.TAIL.BREAKPOINT : load.progress;
+//     // загрузка всей страницы (всех изображений)
+//     $(window).on("load", () => {
+//       load.progress = load.progress <= 0 ? load.TAIL.BREAKPOINT : load.progress;
 
-      if (load.progress >= 1) {
-        preloaderClose();
-      } else {
-        load.endTime = new Date().getTime();
+//       if (load.progress >= 1) {
+//         preloaderClose();
+//       } else {
+//         load.endTime = new Date().getTime();
 
-        load.time = load.endTime - load.startTime;
-        load.time = load.time > 0 ? load.time : 0;
+//         load.time = load.endTime - load.startTime;
+//         load.time = load.time > 0 ? load.time : 0;
 
-        const tail = {
-          time: null,
-          partCounter: 0,
-          partTime: null,
-          interval: null,
-        };
+//         const tail = {
+//           time: null,
+//           partCounter: 0,
+//           partTime: null,
+//           interval: null,
+//         };
 
-        tail.time = (load.time / load.progress) * (1 - load.progress);
-        tail.time =
-          tail.time < load.TAIL.MAX_TIME
-            ? tail.time < load.TAIL.MIN_TIME
-              ? load.TAIL.MIN_TIME
-              : tail.time
-            : load.TAIL.MAX_TIME;
+//         tail.time = (load.time / load.progress) * (1 - load.progress);
+//         tail.time =
+//           tail.time < load.TAIL.MAX_TIME
+//             ? tail.time < load.TAIL.MIN_TIME
+//               ? load.TAIL.MIN_TIME
+//               : tail.time
+//             : load.TAIL.MAX_TIME;
 
-        tail.partCounter = 0;
-        tail.partTime = tail.time / load.TAIL.PARTS;
-        tail.interval = setInterval(() => {
-          tail.partCounter++;
+//         tail.partCounter = 0;
+//         tail.partTime = tail.time / load.TAIL.PARTS;
+//         tail.interval = setInterval(() => {
+//           tail.partCounter++;
 
-          if (tail.partCounter < load.TAIL.PARTS) {
-            preloaderUpdate(
-              load.progress +
-                ((1 - load.progress) / load.TAIL.PARTS) * tail.partCounter
-            );
-          } else {
-            preloaderClose();
+//           if (tail.partCounter < load.TAIL.PARTS) {
+//             preloaderUpdate(
+//               load.progress +
+//                 ((1 - load.progress) / load.TAIL.PARTS) * tail.partCounter
+//             );
+//           } else {
+//             preloaderClose();
 
-            clearInterval(tail.interval);
-          }
-        }, tail.partTime);
-      }
-    });
+//             clearInterval(tail.interval);
+//           }
+//         }, tail.partTime);
+//       }
+//     });
 
-    // обновление прелоадера (отображение прогресса)
-    function preloaderUpdate(loadProgress) {
-      const loadProgressPercent = Math.round(loadProgress * 100);
+//     // обновление прелоадера (отображение прогресса)
+//     function preloaderUpdate(loadProgress) {
+//       const loadProgressPercent = Math.round(loadProgress * 100);
 
-      const offset =
-        circumference - (loadProgressPercent / 100) * circumference;
-      circle.css("strokeDashoffset", offset);
-    }
+//       const offset =
+//         circumference - (loadProgressPercent / 100) * circumference;
+//       circle.css("strokeDashoffset", offset);
+//     }
 
-    // закрытие прелоадера
-    function preloaderClose() {
-      $(".loaded-content").css("display", "block");
-      circle.css("strokeDashoffset", 0);
+//     // закрытие прелоадера
+//     function preloaderClose() {
+//       $(".loaded-content").css("display", "block");
+//       circle.css("strokeDashoffset", 0);
 
-      // OLD CODE START
+//       // OLD CODE START
 
-      $("body").css("overflow", "visible");
+//       $("body").css("overflow", "visible");
 
-      setTimeout(() => {
-        $(".loader").addClass("hidden");
-        AOS.init({ offset: 50 });
-      }, 500);
+//       setTimeout(() => {
+//         $(".loader").addClass("hidden");
+//         AOS.init({ offset: 50 });
+//       }, 500);
 
-      if ($(".rellax").length) {
-        // eslint-disable-next-line no-new
-        new Rellax(".rellax");
-      }
+//       if ($(".rellax").length) {
+//         // eslint-disable-next-line no-new
+//         new Rellax(".rellax");
+//       }
 
-      // OLD CODE END
-    }
-  } else {
-    $("body").css("overflow", "hidden");
+//       // OLD CODE END
+//     }
+//   } else {
+//     $("body").css("overflow", "hidden");
 
-    setTimeout(() => {
-      $("body").css("overflow", "visible");
+//     setTimeout(() => {
+//       $("body").css("overflow", "visible");
 
-      AOS.init({ offset: 50 });
+//       AOS.init({ offset: 50 });
 
-      if ($(".rellax").length) {
-        // eslint-disable-next-line no-new
-        new Rellax(".rellax");
-      }
-    }, 500);
-  }
-});
+//       if ($(".rellax").length) {
+//         // eslint-disable-next-line no-new
+//         new Rellax(".rellax");
+//       }
+//     }, 500);
+//   }
+// });
 
 // {
 // 	$(() => {
---
+++ src/assets/scripts/scroll.js
@@ -0,0 +1,22 @@
+let scrollPrev = 0;
+
+$(window).on("scroll", () => {
+  changeHeaderBg();
+});
+
+const changeHeaderBg = () => {
+  const header = $(".header");
+
+  if ($(window).scrollTop() > $(window).height() / 4) {
+    header.addClass("scrolled");
+  } else header.removeClass("scrolled");
+
+  if (
+    $(window).scrollTop() > $(window).height() / 3 &&
+    $(window).scrollTop() > scrollPrev
+  )
+    header.addClass("out");
+  else header.removeClass("out");
+
+  scrollPrev = $(window).scrollTop();
+};
